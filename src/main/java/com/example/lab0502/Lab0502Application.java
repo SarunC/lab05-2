db6fafdcfd13795a8b1fd45143da0c770a5f44fa
SarunC <63070164@kmitl.ac.th>
2022-09-20 12:17:32 +0700

    test

---
+++ src/main/java/com/example/lab0502/Lab0502Application.java
@@ -0,0 +1,13 @@
+package com.example.lab0502;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab0502Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab0502Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lab0502/Lab0502ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab0502;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab0502ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
